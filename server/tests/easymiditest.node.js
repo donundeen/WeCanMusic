515d9eee26ec893a2ac94edc14d648a8e571f9f4
don undeen <donundeen@yahoo.com>
2024-07-14 17:17:05 -0400

    new notes

--- server/tests/easymiditest.node.js
+++ server/tests/easymiditest.node.js
@@ -30,7 +30,7 @@ setTimeout(function(){
 }, 10);
 
 setTimeout(function(){
-    midiSetBankProgram(1, "001", 3);
+    midiSetBankProgram(1, "002", 3);
     midiMakeNote(1, 65, 120, 500);
     midiMakeNote(1, 69, 120, 500);
     midiMakeNote(1, 73, 120, 500);
