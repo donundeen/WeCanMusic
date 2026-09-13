757d08dee42f627fd13de277bb1186b9c041f280
don undeen <donundeen@yahoo.com>
2026-09-13 18:09:01 -0400

    edp work

--- server/allmidioff.node.js
+++ server/allmidioff.node.js
@@ -1,13 +1,17 @@
 let useMidiOut = true; // whether or not to send midi values through a hardware output, via easymidi
 //let midiOutPortname = "UM-ONE";
 
+// use config file to set the waitFor portname
+
+const Config = require('./configs/conductor.config.js');
+config = new Config();
 const Debugging = require('./modules/debugging.module.js');
 // TURN DEBUGGING ON/OFF HERE
 db = new Debugging();
 
 
-const MidiOuts = require('./modules/midiouts.module.js');
-midiOuts = new MidiOuts({db:db, active: useMidiOut, matches: "all", waitFor : ["UM-ONE","Port 1"]});
+const MidiOuts = require('./modules/midi.midiouts.module.js');
+midiOuts = new MidiOuts({db:db, active: useMidiOut, matches: "all", waitFor : config.waitForPortname});
 midiOuts.init();
 midiOuts.send("reset");
 
